7cffe73ec1dcc7c230b75dd42d8d6c837668f6ed
Marcel Balint <marcelbalint363@gmail.com>
2024-01-29 13:56:40 +0200

    Styled the question card and added icons

--- src/components/chat/DisplayQuestion.js
+++ src/components/chat/DisplayQuestion.js
@@ -1,4 +1,7 @@
 import React from "react";
+import answerIcon from "../../images/answer-icon.svg";
+import editIcon from "../../images/edit-icon.svg";
+import { Likes } from "./Likes";
 
 import "./DisplayQuestion.css";
 import Answer from "./Answer";
@@ -6,8 +9,19 @@ import Answer from "./Answer";
 const Question = (props) => {
   return (
     <div className="question">
+      <p className="edit-icon">
+        <img src={editIcon} alt="Edit" />
+        <span>Edit</span>
+      </p>
       <h3 className="question-title">{props.title}</h3>
       <p className="question-definition">{props.text}</p>
+      <div className="question-reactions">
+        <p className="reaction-answer">
+          <img src={answerIcon} alt="Answer" /> <span>Answer</span>
+        </p>
+        <Likes />
+      </div>
+
       {/* <Answer answer="The first answer" /> */}
     </div>
   );
--- src/components/chat/Likes.js
+++ src/components/chat/Likes.js
@@ -1,16 +1,25 @@
 import React, { useState } from "react";
+import emptyLike from "../../images/like-empty-icon.svg";
+import filledLike from "../../images/like-filled-icon.svg";
+import "./Likes.css";
 
 export function Likes(props) {
   const [likes, setLikes] = useState(0);
 
   const handleLike = () => {
-    setLikes(likes + 1);
+    likes === 1 ? setLikes(0) : setLikes(1);
   };
 
   return (
     <>
-      <p>Likes: {likes}</p>
-      <button onClick={handleLike}>I Like</button>
+      <p className="reaction-like">
+        <img
+          src={likes ? filledLike : emptyLike}
+          onClick={handleLike}
+          alt="Like"
+        />
+        <span> {likes}</span>
+      </p>
     </>
   );
 }
